2954dffbfa3f196c9d4930c30b3d24893ecf0ca5
3bdoredaa2244 <ar2244@fayoum.edu.eg>
2023-05-27 14:19:22 +0200

    main

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:record/record.dart';
-
 void main() {
   runApp(const MaterialApp(home: MicPage()));
 }
 
+
 class MicPage extends StatefulWidget {
   const MicPage({super.key});
+  
 
   @override
   State<MicPage> createState() => _MicPageState();
@@ -19,9 +20,11 @@ class _MicPageState extends State<MicPage> {
   Timer? timer;
   List<double> volumeList = [];
 
-  double volume = 0.0;
+  static double volume = 0.0;
   double minVolume = -45.0;
-
+  double average = 0.0;
+  double total=0;
+  int counter=0;
   startTimer() async {
     timer ??= Timer.periodic(
         const Duration(milliseconds: 50), (timer) => updateVolume());
@@ -30,6 +33,7 @@ class _MicPageState extends State<MicPage> {
   updateVolume() async {
     Amplitude ampl = await myRecording.getAmplitude();
     if (ampl.current > minVolume) {
+
       setState(() {
         volume = (ampl.current - minVolume) / minVolume;
         volumeList.add(volume);
@@ -38,28 +42,37 @@ class _MicPageState extends State<MicPage> {
   }
 
   int volume0to(int maxVolumeToDisplay) {
+    if(counter ==10)
+    {
+      average =total/10;
+      counter=0;
+    }
+    else{
+      total=volume+total;
+      counter =counter+1;
+    }
     return (volume * maxVolumeToDisplay).round().abs();
   }
 
-  double calculateAverage(List<double> volumes) {
-    double sum = 0.0;
-    volumes.forEach((volume) {
-      sum += volume;
-    });
-    return sum / volumes.length;
-  }
+  // double calculateAverage(List<double> volumes) {
+  //   double sum = 0.0;
+  //   volumes.forEach((volume) {
+  //     sum += volume;
+  //   });
+  //   return sum / volumes.length;
+  // }
 
-  void compareWith70() {
-    double averageVolume = calculateAverage(volumeList);
-    if (averageVolume >= 70.0) {
-      print(
-          "The average volume is ${averageVolume.toStringAsFixed(2)} - Too loud!");
-    } else {
-      print(
-          "The average volume is ${averageVolume.toStringAsFixed(2)} - Safe volume.");
-    }
-    volumeList.clear();
-  }
+  // void compareWith70() {
+  //   double averageVolume = calculateAverage(volumeList);
+  //   if (averageVolume >= 70.0) {
+  //     print(
+  //         "The average volume is ${averageVolume.toStringAsFixed(2)} - Too loud!");
+  //   } else {
+  //     print(
+  //         "The average volume is ${averageVolume.toStringAsFixed(2)} - Safe volume.");
+  //   }
+  //   volumeList.clear();
+  // }
 
   Future<bool> startRecording() async {
     if (await myRecording.hasPermission()) {
@@ -69,7 +82,7 @@ class _MicPageState extends State<MicPage> {
       startTimer();
       //calculateAverage();
       Timer.periodic(const Duration(seconds: 10), (timer) {
-        compareWith70();
+       // compareWith70();
       });
       return true;
     } else {
@@ -89,12 +102,17 @@ class _MicPageState extends State<MicPage> {
         builder: (context, AsyncSnapshot<bool> snapshot) {
           return Scaffold(
             body: Center(
-                child: snapshot.hasData
-                    ? Text("VOLUME NOISE\n${volume0to(100)}",
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                            fontSize: 42, fontWeight: FontWeight.bold))
-                    : const CircularProgressIndicator()),
+                child: Column(
+                  children: [
+                    snapshot.hasData
+                        ? Text("VOLUME NOISE\n${volume0to(100)}",
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                                fontSize: 42, fontWeight: FontWeight.bold))
+                        : const CircularProgressIndicator(),
+                        Text("average = ${average}")
+                  ],
+                )),
           );
         });
   }
